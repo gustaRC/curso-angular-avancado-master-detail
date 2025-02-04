b7f7051c922bfaf606f698e5189fca6cbfa04e89
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-02-04 15:26:42 -0300

    Adicionado funcionalidade de submit e update formulário categorias

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -5,6 +5,8 @@ import { Category } from '../shared/category.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 
+import toastr from 'toastr';
+
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -37,6 +39,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm() {
+    this.submittingForm = true;
+    if (this.currentAction == 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
+  }
+
   //PRIVATE METHODS
   private setCurrentAction() {
     this.route.snapshot.url[0].path == 'new' ?
@@ -56,7 +67,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     if (this.currentAction == "edit") {
       this.route.paramMap
       .pipe(
-        switchMap(params => this.categoryService.getById(+params.get('id')))
+        switchMap(params => this.categoryService.getById(+params.get('id'))) //transformando o id em number
       )
       .subscribe(
         (category) => {
@@ -77,5 +88,63 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       this.pageTitle = 'Editando Categoria: ' + categoryName;
     }
   }
+
+  private createCategory() {
+    const category: Category = Object.assign(
+      new Category(),
+      this.categoryForm.value
+    ); //cria um objeto vazio e atribui os valores do form
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionForSucess(category),
+        error => this.actionsForError(error)
+      )
+
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(
+      new Category(),
+      this.categoryForm.value
+    ); //cria um objeto vazio e atribui os valores do form
+
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionForSucess(category),
+        error => this.actionsForError(error)
+      )
+
+  }
+
+  private actionForSucess(category) {
+    toastr.success('Solicitação processada com sucesso!');
+
+    //force reload component page
+    //1. categories/new | 2. categories/ | 3. categories/:id/edit
+
+    this.router.navigateByUrl( "categories", {
+        skipLocationChange: true
+        /* não adicionará no historico de navegação do navegador,
+        se o usuário clicar em retornar no navegador, não irá voltar pra está página,
+        pois ela não foi armazenada no histórico */
+    }).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+
+  }
+
+  private actionsForError(error) {
+    toastr.error("Ocorreu um erro ao procressar a sua solicitação!");
+
+    this.submittingForm = false;
+
+    if(error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor, tente mais tarde.'];
+    }
+  }
+
 
 }
