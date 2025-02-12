85a628b0899004346bb9ef0e17daa4258d643506
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-02-12 14:35:35 -0300

    Criado classe BaseResourceService para requisições default de serviços. Integrado base-resource.service no category.service

---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,87 @@
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { BaseResourceModel } from "../models/base-resource.model";
+import { Injector } from '@angular/core';
+
+export abstract class BaseResourceService<T extends BaseResourceModel> { //abstrata pois não vai ser instanciada
+  //Generics => T significa a classe que está sendo trabalhada em cada service.
+  //Exemplo: Category na CategoryService, Entry no EntryService
+
+  protected http: HttpClient;
+
+  constructor(
+    protected apiPath: string, //requisição do in-memory-database
+    protected injector: Injector
+  ){
+    this.http = injector.get(HttpClient);
+  }
+
+  getAll(): Observable<T[]> {
+    return this.http.get<T[]>(this.apiPath)
+      .pipe(
+        catchError(this.handleError),
+        map(this.jsonDataToResources)
+      );
+  }
+
+  getById(id: number): Observable<T> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get<T>(url)
+      .pipe(
+        catchError(this.handleError),
+        map(this.jsonDataToResource)
+      );
+  }
+
+  create(resource: T): Observable<T> {
+    return this.http.post<T>(this.apiPath, resource)
+      .pipe(
+        catchError(this.handleError),
+        map(this.jsonDataToResource)
+      );
+  }
+
+  update(resource: T): Observable<T> {
+    const url = `${this.apiPath}/${resource.id}`;
+
+    return this.http.put<T>(url, resource)
+      .pipe(
+        catchError(this.handleError),
+        map(() => resource)
+      );
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url)
+      .pipe(
+        catchError(this.handleError),
+        map(() => null)
+      );
+  }
+
+  //PROTECTED METHODS
+  //torna visivil somente na classe base e em suas extensões
+  //caso fosse private, não seria possivel as extensões utilizarem os recursos
+
+  protected jsonDataToResources(jsonData: any[]): T[] {
+    const resources: T[] = [];
+    jsonData.forEach(element => resources.push(element as T));
+    return resources;
+  }
+
+  protected jsonDataToResource(jsonData: any): T {
+    return jsonData as T;
+  }
+
+  protected handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO => ', error);
+    return throwError(error);
+  }
+
+}
