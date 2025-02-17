97a2ce44e6d74ac2b440555139a9d86779f6a04b
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-02-17 11:41:46 -0300

    Criado função auxiliar para otmização dos métodos do EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { flatMap } from 'rxjs/operators';
+import { catchError, flatMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -22,28 +22,24 @@ export class EntryService extends BaseResourceService<Entry>{
   }
 
   create(entry: Entry): Observable<Entry> {
-    //configuração do Category no objeto Entry
-    return this.categoryService.getById(entry.categoryId)
-      .pipe(
-        flatMap(category => { //flatMap substituido pelo mergeMap
-          entry.category = category;
-
-          //post entry
-          return super.create(entry); //CHAMANDO O METODO DA CLASSE PAI
-        })
-      )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this)); //super - Não está sendo executado! Sendo passado somente a sintaxe
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this)); //super - Não está sendo executado! Sendo passado somente a sintaxe
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     //configuração do Category no objeto Entry
     return this.categoryService.getById(entry.categoryId)
       .pipe(
-        flatMap(category => { //flatMap substituido pelo mergeMap
+        flatMap(category => {
           entry.category = category;
 
-          //put entry
-          return super.update(entry); //CHAMANDO O METODO DA CLASSE PAI
-        })
+          //post entry
+          return sendFn(entry); //CHAMANDO O METODO DA CLASSE PAI
+        }),
+        catchError(this.handleError) //metodo da classe BASE
       )
   }
 
