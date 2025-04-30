375f508f420347d20a811ad4924a7d2ed3e37251
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-04-30 14:34:44 -0300

    Criado componente para exibir o header das paginas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCumpComponent } from './components/bread-cump/bread-cump.component';
 import { RouterModule } from '@angular/router';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCumpComponent],
+  declarations: [BreadCumpComponent, PageHeaderComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -15,7 +16,8 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    BreadCumpComponent
+    BreadCumpComponent,
+    PageHeaderComponent
   ]
 })
 export class SharedModule { }
