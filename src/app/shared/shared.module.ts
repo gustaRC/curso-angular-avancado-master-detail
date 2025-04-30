9a85114e969b4db99257b71abb965ec5f6686a7a
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-04-30 12:29:30 -0300

    Adicionado RouterModule no imports e exports do SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCumpComponent } from './components/bread-cump/bread-cump.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BreadCumpComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
     BreadCumpComponent
   ]
 })
