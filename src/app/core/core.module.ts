45253702684bac46c9e182fdb838d9d8c0584fad
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-05-09 12:28:50 -0300

    Criado componente para exibir o navbar da aplicação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,21 +6,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   exports: [
     //shared modules
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
